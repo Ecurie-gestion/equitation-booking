1cacba9d921c76375cffa740b0dccdac81af21e3
Ecuire-de-Groynne <ecuriedegroynne1@gmail.com>
2026-08-24 23:59:46 +0200

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://afqvwvbmmlzhnosbhvnu.supabase.co',
-sb_publishable_kEZYZpMmaMVJyIGZUWEPqQ_REh1EsUs
+'sb_publishable_kEZYZpMmaMVJyIGZUWEPqQ_REh1EsUs'
 )
